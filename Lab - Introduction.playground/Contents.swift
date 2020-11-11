e25b0471ed37ada9c90a0ba12e429afa0cea6ff2
Easy5050 <74300394+Easy5050@users.noreply.github.com>
2020-11-11 23:52:58 +0300

    Update Contents.swift

--- Lab - Introduction.playground/Contents.swift
+++ Lab - Introduction.playground/Contents.swift
@@ -12,11 +12,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("the song title: School")
+print("the artist is : Supertramp")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("I can see you in the morning when you go to school")
+print("Don't forget your books, you know you've got to learn the golden rule,")
+print("Teacher tells you stop your play and get on with your work")
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
